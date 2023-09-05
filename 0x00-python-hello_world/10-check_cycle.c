c7c97db500bc9edf9dc84a104923f28ebbdb02a7
njine <brikab@gmail.com>
2023-09-05 04:59:53 +0300

    9

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,20 +7,27 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *check;
+    listint_t *current, *check;
 
-	if (list == NULL || list->next == NULL)
-		return (0);
-	current = list;
-	check = current->next;
+    if (list == NULL || list->next == NULL)
+        return (0);
 
-	while (current != NULL && check->next != NULL
-		&& check->next->next != NULL)
-	{
-		if (current == check)
-			return (1);
-		current = current->next;
-		check = check->next->next;
-	}
-	return (0);
+    current = list;
+    check = current->next;
+
+    do
+    {
+        if (current == check)
+            return (1);
+
+        current = current->next;
+
+        if (check->next != NULL && check->next->next != NULL)
+            check = check->next->next;
+        else
+            return (0);
+
+    } while (current != NULL);
+
+    return (0);
 }
