4b9f222796f8474d4203a75beb9d772b03a3fefd
njine <brikab@gmail.com>
2023-09-06 05:08:32 +0300

    saba

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python3
-import random
 
-# Generate a random number
-number = random.randint(-10000, 10000)
-
-# Extract the last digit and handle negative numbers
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-
-# Define the output components
-output = ["Last digit of", str(number), "is", str(digit), "and is"]
-
-# Determine the last part of the output
-if digit > 5:
-    output.append("greater than 5")
-elif digit == 0:
-    output.append("0")
-else:
-    output.append("less than 6 and not 0")
-
-# Print the formatted output
-print(" ".join(output))
+# Loop through numbers from 0 to 98
+for number in range(99):
+    # Use f-strings to format the output
+    print(f"{number} = 0x{number:02x}")
