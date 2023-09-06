8462b7226627a174619cd1eb3e77d10b9a3403f4
njine <brikab@gmail.com>
2023-09-06 05:56:37 +0300

    tisa

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{:c}".format(i - 32), end="")
+    else:
+        print("{:c}".format(i), end="")
+
     if i % 2:
         print("{:c}".format(i - 32), end="")
     else:
