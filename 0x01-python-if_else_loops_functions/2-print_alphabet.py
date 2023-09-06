69b924234de71497b8cbadcb7520d5b8f75ee5dd
njine <brikab@gmail.com>
2023-09-06 04:50:55 +0300

    saba

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-"""Print the alphabet in lowercase, not followed by a new line."""
-alphabet = ""
-for letter in range(97, 123):
-    alphabet += chr(letter)
-
-print(alphabet, end="")
+"""Print alphabet in lowercase without a new line."""
+print("".join([f"{chr(letter)}" for letter in range(97, 123)]), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
+import random
 
-"""Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(99):
-    print("{} = 0x{:02x}".format(number, number))
+# Generate a random number
+number = random.randint(-10000, 10000)
+
+# Extract the last digit and handle negative numbers
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+
+# Define the output components
+output = ["Last digit of", str(number), "is", str(digit), "and is"]
+
+# Determine the last part of the output
+if digit > 5:
+    output.append("greater than 5")
+elif digit == 0:
+    output.append("0")
+else:
+    output.append("less than 6 and not 0")
+
+# Print the formatted output
+print(" ".join(output))
