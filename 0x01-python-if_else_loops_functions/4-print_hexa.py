26ee2bed993dbfe40870bbdcb1d9a9165b0c3e7c
njine <brikab@gmail.com>
2023-09-06 05:12:52 +0300

    saba

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,4 +2,4 @@
 
 # Use a single loop to print numbers in both decimal and hexadecimal format
 for number in range(99):
-    print("{} = 0x{:02x}".format(number, number))
+    print("{} = 0x{:x}".format(number, number))
