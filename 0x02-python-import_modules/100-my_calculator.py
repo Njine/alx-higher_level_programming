f0bce950e8700154f49017ab169791ecedc832be
njine <brikab@gmail.com>
2023-09-08 02:12:59 +0300

    calc100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
-def main():
-    if len(sys.argv) != 4:
-        print('Usage: {} <a> <operator> <b>'.format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
 
-    num1 = int(sys.argv[1])
-    operator = sys.argv[2]
-    num2 = int(sys.argv[3])
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
     ops = {
         '+': add,
@@ -19,11 +19,8 @@ def main():
     }
 
     if operator in ops:
-        result = ops[operator](num1, num2)
-        print('{:d} {:s} {:d} = {:d}'.format(num1, operator, num2, result))
+        result = ops[operator](a, b)
+        print('{:d} {:s} {:d} = {:d}'.format(a, operator, b, result))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+        exit(1)
