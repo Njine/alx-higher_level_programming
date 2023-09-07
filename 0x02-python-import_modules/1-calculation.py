43ea380c2420ab5705ebb3979d25d06d76b24b19
njine <brikab@gmail.com>
2023-09-08 01:30:33 +0300

    1-calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-def perform_arithmetic_operations(a, b):
-    """Perform arithmetic operations on two numbers and print the results."""
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    perform_arithmetic_operations(a, b)
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))
