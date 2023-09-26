88314c42073b85468a42a1890eac50639ea9b460
njine <brikab@gmail.com>
2023-09-27 00:14:30 +0300

    21

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""2-square.py: Defines size as an int and also greater or equal to 0."""
+"""2-square.py: Defines size as an int and also >= 0."""
 
 
 class Square:
-    """Creates  Square type."""
+    """Create Square type."""
 
     def __init__(self, dimention=0):
         """Initialize Square with size."""
