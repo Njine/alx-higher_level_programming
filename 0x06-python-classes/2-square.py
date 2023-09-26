949a107dad5a64efdbfc930547d45885a7c969c2
njine <brikab@gmail.com>
2023-09-27 00:25:11 +0300

    21

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""2-square.py: Defines size as an int and also >= 0."""
+"""2-square.py: Defines size as an int and also greater than 0."""
 
 
 class Square:
     """Creates  Square type."""
 
-    def __init__(self, size=0):
-        """Initialize Square with size."""
-        self.__size = size
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+    def __init__(self, dime=0):
+        """Initialize Square with dime."""
+        self.__dime = dime
+        if type(dime) is not int:
+            raise TypeError('dime must be an integer')
+        if dime < 0:
+            raise ValueError('dime must be >= 0')
