812c81607018cd6bea900104eaf16888b74e5ee7
njine <brikab@gmail.com>
2023-09-27 03:17:46 +0300

    55

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""Prints to stdout the square with the character # ."""
+"""Print to stdout the square with the character # ."""
 
 
 class Square:
-    """Create Square type."""
-
+    """Create  Square type."""
     def __init__(self, dime=0):
         """Initialize Square with size."""
         self.size = dime
 
     @property
-    def dime(self):
-        """Define size of square and returns value."""
+    def size(self):
+        """Define the size of square and returns its value."""
         return self.__size
 
-    @dime.setter
-    def dime(self, worth):
+    @size.setter
+    def size(self, param):
         """Define the value of size of square and checks if >= 0."""
-        self.__size = worth
-        if type(worth) is not int:
+        self.__size = param
+        if type(param) is not int:
             raise TypeError('size must be an integer')
-        if worth < 0:
+        if param < 0:
             raise ValueError('size must be >= 0')
 
     def area(self):
-        """Define area of a square."""
+        """Define the area of a square."""
         return self.__size * self.__size
 
     def my_print(self):
-        """Print in stdout the square with character #."""
-        for a in range(self.__size):
-            for b in range(self.__size):
-                print('#', end="")
-            print()
-        if self.__size == 0:
-            print()
+    """Print in stdout the square with character #."""
+    if self.__size == 0:
+        print()
+    else:
+        for _ in range(self.__size):
+            print('#' * self.__size)
