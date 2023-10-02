1168e45a391081151770ed2a077a97b3592cd9b8
njine <brikab@gmail.com>
2023-10-02 20:51:52 +0300

    sit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''defines an empty class called Rectangle'''
+
+
+class Rectangle:
+    '''an empty Rectangle type is made'''
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''It defines Rectangle type'''
+
+
+class Rectangle:
+    '''Rectangle type is defined'''
+
+    def __init__(self, height=0, width=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+        
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
