6272ad0854227b435714d129c970d2fd09285014
njine <brikab@gmail.com>
2023-10-04 00:57:43 +0300

    msq

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Return a string “BestSchool” n times the number of the iteration."""
-
-
 def magic_string(l=[]):
-    """Return string 'Holberton' repeated based on the no of function calls."""
     l += ["Holberton"]
     return ", ".join(l)
