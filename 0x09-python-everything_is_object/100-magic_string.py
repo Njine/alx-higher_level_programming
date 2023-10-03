c03434836ac377167bf62a8947f7260caf50cda2
njine <brikab@gmail.com>
2023-10-04 00:49:27 +0300

    msa

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Return a string “BestSchool” n times the number of the iteration."""
 
-def magic_string(iteration=[0]):
-    iteration[0] += 1
-    return "BestSchool, " * (iteration[0] - 1) + "BestSchool"
 
-for i in range(10):
-    print(magic_string())
-
+def magic_string(l=[]):
+    """Return string 'Holberton' repeated based on the no of function calls."""
+    l += ["Holberton"]
+    return ", ".join(l)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""
-This is a module that containts a clas that avoids
-dynmaically created attributes
-"""
+"""Class that avoids dynmaically created attributes."""
+
+
 class LockedClass:
+    """Prevent the user from dynamically creating new instance attributes."""
+
     __slots__ = ['first_name']
 
     def __init__(self):
-        """ Init method """
+        """Initialize method."""
         pass
