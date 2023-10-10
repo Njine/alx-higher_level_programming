da81877f340d70eb016f72c6168aadb0e7c8fe14
njine <brikab@gmail.com>
2023-10-11 01:10:20 +0300

    me

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,9 +6,9 @@ class Student:
     """Student class."""
     def __init__(self, first_name, last_name, age):
         """Initialize student class."""
-        self.firstName = first_name
-        self.lastName = last_name
-        self.studentAge = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """Convert file to JSON."""
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Create class Student"""
+"""Create class Student."""
 
 
 class Student:
     """Student class."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize student class."""
-        self.firstName = first_name
-        self.lastName = last_name
-        self.studentAge = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
-        """Convert file to JSON."""
+        """Convert object attributes to JSON."""
         if attrs is None:
             return self.__dict__
-        my_dict = {}
-        for items in attrs:
-            if hasattr(self, items):
-                my_dict[items] = getattr(self, items)
-        return my_dict
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
     def reload_from_json(self, json):
-        """Reload file from JSON."""
-        for items in json:
-            self.__dict__[items] = json[items]
+        """Reload object attributes from JSON dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
