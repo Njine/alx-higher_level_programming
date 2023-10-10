7660aff226142790b5ad0a2bb7f743ae259814bf
njine <brikab@gmail.com>
2023-10-10 03:29:39 +0300

    vol2

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Rectangle class."""
 
-
+# Import the BaseGeometry class from the 7-base_geometry module
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
 class Rectangle(BaseGeometry):
-    """class Rectangle inherits from BaseGeometry"""
+    """A class Rectangle that inherits from BaseGeometry"""
 
-    class Rectangle:
     def __init__(self, width, height):
-        self.__width = self.validate_and_set('width', width)
-        self.__height = self.validate_and_set('height', height)
+        """Initialize a Rectangle instance with width and height."""
+        # Call the integer_validator method from the parent class for width
+        super().integer_validator('width', width)
+        # Assign the width as a private instance attribute
+        self.__width = width
+        # Call the integer_validator method from the parent class for height
+        super().integer_validator('height', height)
+        # Assign the height as a private instance attribute
+        self.__height = height
 
-    def validate_and_set(self, name, value):
-        super().integer_validator(name, value)
-        return value
+    def __str__(self):
+        """Return an informal representation of the Rectangle."""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        """Return a formal representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Compute and return the area of the Rectangle."""
+        return self.__width * self.__height
