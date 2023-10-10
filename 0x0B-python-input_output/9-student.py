19ec636aa99091fc5757e0bc5ad57f2345aff6df
njine <brikab@gmail.com>
2023-10-11 00:46:47 +0300

    memu

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,8 +7,8 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize student class."""
-        self.firstName = first_name
-        self.lastName = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
