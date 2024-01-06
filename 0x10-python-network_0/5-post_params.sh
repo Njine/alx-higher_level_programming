ce53a9e99e55c81eae95379ed15b1e744270e537
njine <brikab@gmail.com>
2024-01-06 04:34:36 +0300

    postP

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request and displays the response body
-curl -s "$1" -X POST --data-urlencode "email=hr@holbertonschool.com" --data-urlencode "subject=I will always be here for PLD"
+curl -s "$1" -X POST --data-urlencode "email=test@gmail.com" --data-urlencode "subject=I will always be here for PLD"
