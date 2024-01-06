90a7e58cfbe56fd8598d3f96a3e66dd64cde5deb
njine <brikab@gmail.com>
2024-01-06 04:48:05 +0300

    ststus_code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to the specified URL and displays only the status code of the response
-curl -s -X -L HEAD -w "%{http_code}\n" "$1"
+curl -s -X -L HEAD -w "%{http_code}" "$1"
