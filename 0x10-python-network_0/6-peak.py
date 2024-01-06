4455dd779f19ede7423e741ba9c374fbbafa1b3c
njine <brikab@gmail.com>
2024-01-06 04:58:50 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for locating peaks in a list of integers."""
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    length = len(list_of_integers)
+    mid = length // 2
+    li = list_of_integers
+
+    if mid - 1 < 0 and mid + 1 >= length:
+        return li[mid]
+
+    if mid - 1 < 0 or li[mid] >= li[mid + 1]:
+        return li[mid]
+    else:
+        return find_peak(li[mid + 1:]) if li[mid + 1] > li[mid - 1] else find_peak(li[:mid])
