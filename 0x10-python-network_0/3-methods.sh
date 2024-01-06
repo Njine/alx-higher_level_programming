17643717dc6b53c26f4462a397d13693fac47c15
njine <brikab@gmail.com>
2024-01-06 04:21:12 +0300

    method repair

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes an URL and shows the Allowed OPTIONS
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
